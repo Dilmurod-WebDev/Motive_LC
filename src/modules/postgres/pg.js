15f9bb0a50c7825ab1f00faf86c2e793d72ed236
Dilmurod-WebDev <dilmurodnabiev00@gmail.com>
2022-01-28 15:18:40 +0500

    update pg.js

--- src/modules/postgres/pg.js
+++ src/modules/postgres/pg.js
@@ -11,3 +11,33 @@ const init = require('./init')
 const sequelize = new Sequelize(process.env.DB_URL, {
     logging: false
 })
+
+
+async function postgres(){
+    try {
+        await sequelize.authenticate();
+
+        let db = {};
+
+        await instantiate(db).then(async () => {
+            await relations(db)
+            await init(db)
+        })
+
+        await sequelize.sync({ force: false })
+
+        return db
+    } catch (error) {
+        console.log("DATABASE_ERROR", error);
+    }
+}
+
+async function instantiate(db) {
+    db.users = await UsersModel(sequelize, Sequelize)
+    db.sessions = await SessionsModel(sequelize, Sequelize)
+    db.groups = await GroupsModel(sequelize, Sequelize)
+    db.courses = await CoursesModel(sequelize, Sequelize)
+}
+
+
+module.exports = postgres;
